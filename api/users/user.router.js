ce45af40a6bc3f02b8ba230e1a3033dddd088442
raorajat21 <rao.rajat@northeastern.edu>
2022-10-04 23:11:15 -0400

    adding user services and routes

---
+++ api/users/user.router.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+const { createUser, getUser, getUserByUserID, updateUser, deleteUser } = require("./user.controller");
+const router = require("express").Router();
+
+router.post("/", createUser);
+router.get("/", getUser);
+router.get("/:id", getUserByUserID);
+router.patch("/:id", updateUser);
+router.delete("/:id", deleteUser);
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,54 +1,24 @@
+require('dotenv').config();
 const express = require('express');
 var bodyParser = require('body-parser');
-
-const mysql = require('mysql');
-
-var mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'admin',
-    password: 'Rajatrao@0594',
-    database: 'admindb'
-});
-
-mysqlConnection.connect((err)=> {
-    if(!err)
-    console.log('DB Connection Established!!');
-    else
-    console.log('DB Connection failed \n Error' + JSON.stringify(err, undefined, 2));
-});
-
 const app = express()
+
+const userRouter = require("./api/users/user.router");
+
 app.use(bodyParser.urlencoded({extended: false}))
+//app.use(express.json());
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
+app.get("/api", (req,res) =>{
+    res.json({
+        success:1,
+        Message: "Rest Api is working"
+    });
+})
 
-const port= 9000;
+app.use("/api/user", userRouter);
 
-app.get("/", (req,res) => {
-    res.json(cloths)
-});
-
-app.get("/cloths/:id", (req,res) => {
-    res.json(cloths[parseInt(req.params.id) -1])
-});
-
-app.post("/cloths", (req,res) => {
-    console.log(req.body)
-    res.json({message: "OK"})
-});
-
-app.put("/cloths/:id", (req,res) => {
-    console.log(req.params.id)
-    console.log(req.body)
-    res.json({ message : `Updating Item: ${req.params.id}`})
-});
-
-app.delete("/cloths/:id", (req,res) => {
-    console.log(req.param.id)
-    res.json({ message: `Deleting Item: ${req.params.id}`})
-});
-
-app.listen(port, ()=> {
-    console.log(`Listening on port ${port}`)
+app.listen(process.env.APP_PORT, ()=> {
+    console.log(`Listening on port ${process.env.APP_PORT}`)
     console.log("Here I am")
 });
