eb423cf60863b815a379534baae4f8b619794fb4
raorajat21 <rao.rajat@northeastern.edu>
2022-11-30 12:45:41 -0500

    modified pkr file

--- setup.sh
+++ setup.sh
@@ -13,24 +13,12 @@ sudo apt-get -y install npm
 sudo apt-get install -y gcc g++ make
 sudo npm i pm2 -g
 sleep 10
-echo "Installing AWS CLI"
-sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-sleep 10
-sudo aws --version
 pwd
 echo "Before installing CW"
 #Install CLOUD WATCH AGENT
 sudo curl -o /root/amazon-cloudwatch-agent.deb https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E /root/amazon-cloudwatch-agent.deb
 echo "Creating Launch Template"
-aws ec2 create-launch-template-version \
---launch-template-id $TEMPLATE_ID \
---version-description webapp2\
---source-version 1 \
---launch-template-data "ImageId=ami-065557360af5257b3" \
---region us-east-1
 sleep 10
 unzip webappDEV.zip -d webappDEV
 echo "installing npm"
